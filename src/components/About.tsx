1f12a4b59f7f7455fb6e5a48828d23fb89fc73bc
Virna <virnzlivara@gmail.com>
2025-02-03 23:53:10 +0800

    updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import { JSX } from "react";
+
 export const About = () : JSX.Element => {
     return(
         <section id="about">
@@ -20,7 +22,7 @@ export const About = () : JSX.Element => {
                     </a>
                     <a
                     href="#projects"
-                    className="ml-4 inline-flex text-gray-900 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                     See My Past Work
                     </a>
                 </div>
